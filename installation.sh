58c8c4b3615feed36cd2196549ceef37f0e75362
enkeipr <66286247+enkeipr@users.noreply.github.com>
2022-05-28 18:07:00 -0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 # A simple Bash script
 
+# Colors Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+NC='\033[0m' # No Color
+
 y="noconfirm"
 
-echo ++++++++++++++++++++++++++++++++++++++++++++++
-echo +++++ Excecuting the installation script +++++
-echo ++++++++++++++++++++++++++++++++++++++++++++++
-echo +
-echo +
-echo +
-echo +
-
-echo sudo pacman -Syyuu --$y
+echo -e "${BCyan} ++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "${BCyan} +++++ Excecuting the installation script +++++"
+echo -e "${BCyan} ++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "${BCyan} + @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ +"
+echo -e "${BCyan} + @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ +"
+echo -e "${BCyan} + @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ +"
+echo -e "${BCyan} + @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ +"
+echo -e "${BCyan} ++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "${BYellow}   ||||||||||||||||||||||||||||||||||||||||||"
+echo ""
+echo -e "${BCyan} sudo pacman -Syyuu --$y ${BYellow} <-- ${NC}"
 sudo pacman -Syyuu --$y
-echo sudo pacman -S zsh --$y
+echo -e "${BCyan} sudo pacman -S zsh --$y ${BYellow} <-- ${NC}"
 sudo pacman -S zsh --$y
-echo sudo pacman -S curl --$y
+echo -e "${BCyan} sudo pacman -S curl --$y ${BYellow} <-- ${NC}"
 sudo pacman -S curl --$y
-echo sudo pacman -S git --$y
+echo -e "${BCyan} sudo pacman -S git --$y ${BYellow} <-- ${NC}"
 sudo pacman -S git --$y
-echo sudo pacman -S docker --$y
+echo -e "${BCyan} sudo pacman -S docker --$y ${BYellow} <-- ${NC}"
 sudo pacman -S docker --$y
-echo sudo pacman -S firefox --$y
+echo -e "${BCyan} sudo pacman -S firefox --$y ${BYellow} <-- ${NC}"
 sudo pacman -S firefox --$y
-echo sudo pacman -S code --$y
+echo -e "${BCyan} sudo pacman -S code --$y ${BYellow} <-- ${NC}"
 sudo pacman -S code --$y
-echo sudo pacman -S go --$y
+echo -e "${BCyan} sudo pacman -S go --$y ${BYellow} <-- ${NC}"
 sudo pacman -S go --$y
-echo sudo pacman -S --needed base-devel
+echo -e "${BCyan} sudo pacman -S --needed base-devel ${BYellow} <-- ${NC}"
 sudo pacman -S --needed base-devel
 cd $HOME
-echo git clone https://aur.archlinux.org/paru.git
+echo -e "${BCyan} git clone https://aur.archlinux.org/paru.git ${BYellow} <-- ${NC}"
 git clone https://aur.archlinux.org/paru.git
 cd paru
-echo makepkg -si --$y
+echo -e "${BCyan} makepkg -si --$y ${BYellow} <-- ${NC}"
 makepkg -si --$y
-echo paru --$y
+echo -e "${BCyan} paru --$y ${BYellow} <-- ${NC}"
 paru --$y
-paru dbvisualizer
-cd $HOME
-echo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --$y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --$y
-echo git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-echo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-cd $HOME
-git clone https://github.com/enkeipr/instalation.git
-cd instalation
-mv .zshrc $HOME
+# paru dbvisualizer
